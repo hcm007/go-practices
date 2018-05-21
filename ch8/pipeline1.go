6133f68c741cfb9551e50fed219022f81ec66ab7
hcm007 <hcm007c@gmail.com>
2018-05-21 11:23:36 -0400

    added pipelne2.go

--- ch8/pipeline1.go
+++ ch8/pipeline1.go
@@ -24,6 +24,5 @@ func main(){
 		fmt.Println(<-squares)
 	}
 
-
 }
 
---
+++ ch8/pipeline2.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	natuals := make(chan int)
+	squares := make(chan int)
+
+	go func(){
+		for x :=0; x < 100; x++{
+			natuals <- x
+		}
+		close(natuals)
+	}()
+
+	go func(){
+		for x := range natuals{
+			squares <- x*x
+		}
+		close(squares)
+	}()
+	
+
+	for x := range squares{
+		fmt.Println(x)
+	}
+
+
+}
